fc49c401388f030ac3fcbd1dffe3c928ab8674f5
seadersn <seadersn@gmail.com>
2020-01-02 14:47:31 +0100

    Revert "WIP bring in f2fs for cache and data WIP"
    This reverts commit 387de7f7e90e43a62a64b814c63464ac8a362edc.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,15 +34,15 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
 # Kernel
-#TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/prebuilt/kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/prebuilt/boot.img-zImage
 #TARGET_KERNEL_APPEND_DTB := true
 #BOARD_SEPOLICY_DIRS += device/lge/joan/sepolicy
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_SOURCE := kernel/lge/msm8998/ #lineage-16.0 branch
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CONFIG := arch/arm64/configs/lineageos_h930_defconfig
-KERNEL_TOOLCHAIN_PREFIX := /home/seadersn/bin/toolchains/bin/aarch64-linux-gnu-
+#TARGET_KERNEL_SOURCE := kernel/lge/msm8998 #lineage-16.0 branch
+#TARGET_KERNEL_ARCH := arm64
+#TARGET_KERNEL_HEADER_ARCH := arm64
+#TARGET_KERNEL_CONFIG := lineageos_h930_defconfig
+#KERNEL_TOOLCHAIN_PREFIX := /home/seadersn/bin/toolchains/bin/aarch64-linux-gnu-
 
 # Boot image
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.hardware=joan androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 firmware_class.path=/firmware/image loop.max_part=7 buildvariant=user androidboot.selinux=permissive
@@ -61,7 +61,7 @@ BOARD_FLASH_BLOCK_SIZE := 0x40000
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
+#TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP specific build flags
 #TW_USE_TOOLBOX := true
@@ -97,7 +97,7 @@ TW_CRYPTO_SYSTEM_VOLD_MOUNT := firmware persist-lg encrypt system eksst
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TW_EXCLUDE_TWRPAPP := true
-TW_DEVICE_VERSION := 2
+TW_DEVICE_VERSION := 1
 #TWRP_EVENT_LOGGING := true
 #TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
 
