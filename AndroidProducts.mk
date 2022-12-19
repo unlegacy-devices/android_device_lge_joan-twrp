06a7741e2dc18736cf964e646c31281fde7c7c41
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-12-19 12:15:54 +0100

    Fix error: commands commence before first target.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-PRODUCT_MAKEFILES := \	
-		$(LOCAL_DIR)/twrp_joan.mk
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/twrp_joan.mk
 
 COMMON_LUNCH_CHOICES := twrp_joan-eng
 
